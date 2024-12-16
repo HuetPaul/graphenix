68ba4bc4d67f3c0b71542f656672b96fa42d9d7c
phuet <paul.huet@obspm.fr>
2024-12-16 18:00:17 +0100

    correction bug lecture de Graphique enregistrés avec plus de 10 courbes

---
+++ build/lib/graphenix/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+#from unittest.mock import right
+from graphenix.objet import *
+from graphenix.gui import plot
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='graphenix',
     packages=find_packages(include=['graphenix']),
     version='0.2.0',
-    description='A library to use matplotlib more easily',
+    description='A library to store and replot matplotlib plot easly and reproducibly',
     author='Paul Huet',
     setup_requires=['pytest-runner'],
 )
